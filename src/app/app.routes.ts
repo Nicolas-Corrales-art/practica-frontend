176cfbcff48e5947f5eddf6ea73e9daad84658b4
Nicolas <nicolas.corrales@uab.edu.bo>
2025-10-08 00:10:26 -0400

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+
+        path:'',
+        component:Layout,
+        children:[
+            {
+                path:'',
+                loadComponent:()=>
+                    import('./home/home').then(m=>m.Home)
+            }
+        ]
+    }
+];
